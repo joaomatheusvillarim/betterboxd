d4ace483274245946cb9efbe77dfe97ce503537f
pedroVelosoFernandes <pedro.henrique.veloso.fernandes@ccc.ufcg.edu.br>
2024-04-29 19:07:27 -0300

    .

--- Prolog/App/Util/SortSearch.pl
+++ Prolog/App/Util/SortSearch.pl
@@ -1,8 +1,8 @@
-merge([],[],_).
+erge([],[],_).
 merge(L,[],L).
 merge([],R,R).
-merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios):T],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2):T2],[row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios):T3]): Rating>=Rating2,merge(T,[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2):T2],T3),!.
-merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios):T],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2):T2],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2):T3]):- merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios):T],T2,T3),!.
+merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios)|T],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2)|T2],[row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios)|T3]): Rating>=Rating2,merge(T,[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2)|T2],T3),!.
+merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios)|T],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2)|T2],[row(ID2, Titulo2, Rating2, Generos2, Ano2, Atores2, Diretores2, Comentarios2)|T3]):- merge([row(ID, Titulo, Rating, Generos, Ano, Atores, Diretores, Comentarios)|T],T2,T3),!.
 
 split(Lista, Esquerda, Direita) :-
     length(Lista, Tamanho),
