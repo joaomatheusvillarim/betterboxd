dc86669f6587c4a5afcc559dabc0d3e46d8f6eac
pedroVelosoFernandes <pedro.henrique.veloso.fernandes@ccc.ufcg.edu.br>
2024-04-29 18:25:40 -0300

    getInfos pronto

--- Prolog/App/Util/GetInfos.pl
+++ Prolog/App/Util/GetInfos.pl
@@ -4,21 +4,39 @@ getString(Mensagem, Resposta):-
     read_line_to_codes(user_input, Codes), 
     string_codes(Resposta, Codes).
 
-getInt(Mensagem, Resposta):- 
+getInt(Mensagem, Resposta):-
     getString(Mensagem, String),
     atom_number(String, Resposta).
 
 getUsernameCadastro(R):-
     getString('Digite o seu username: ', R).
+    length(R,Tamanho),
+    (hasUsername(R) -> 
+        writeln('Aviso: O nome do usuário já está em uso.'),
+        getUsernameCadastro(R)
+    ;Tamanho > 18 ->
+        writeln('Aviso: O nome do usuário deve ter no máximo 18 caracteres.'),
+    getUsernameCadastro(R)
+    ).
 
 getnameCadastro(R):-
-    getString('Digite o seu nome: ', R).
+    getString('Digite o seu nome: ', R),
+    length(R,Tamanho),
+    (Tamanho > 18 -> 
+        writeln('Aviso: O seu nome deve ter no máximo 18 caracteres.'),
+        getnameCadastro(R)
+    ).
 
 getBioCadastro(R):-
     getString('Digite a sua bio: ', R).
 
 getPasswordCadastro(R):-
-    getString('Digite a sua senha: ', R).
+    getString('Digite a sua senha: ', R),
+    length(R,Tamanho),
+    (Tamanho < 5 ->
+        writeln('Aviso: A senha deve ter no mínimo 5 digitos.'),
+        getPasswordCadastro(R)
+    ).
 
 getUsernameLogin(R):-
     getString('Digite o seu username: ', R).
@@ -27,7 +45,11 @@ getPasswordLogin(R):-
     getString('Digite a sua senha: ', R).
 
 getNumberStars(R):-
-    getInt('De 0 a 5 estrelas, como você avalia o filme? ', R).
+    getInt('De 0 a 5 estrelas, como você avalia o filme? ', R),
+    (R < 0 ; R > 5 -> 
+        writeln('Aviso: Quantidade de estrelas inválidas'),
+        getNumberStars(R)    
+    ).
 
 getComentario(R):-
     getString('Insira seu comentário: ', R).
